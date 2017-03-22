71160d8a42deb5b361f1d8027ac867d2fb4d75c4
Katarina Ljungdahl <katarina@folkungagatan54.se>
2017-03-22 12:01:37 +0100

    Added fade in for smooth loading of content on index.php, also added a color filter on background image

--- footer.php
+++ footer.php
@@ -1,1 +1,7 @@
+    <script>
+     (function () {
+       document.documentElement.classList.add('has-js');
+     }());
+    </script>
+  </body>
 </html>
--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 <?php @include 'header.php'; ?>
 <body class="front">
-  <a class="navigation__link link" href="">Menu</a>
-  <img class="symbol symbol--top-left" src="assets/img/symbol.png" alt="Cult of Bulldada symbol">
-  <img class="symbol symbol--top-right"src="assets/img/symbol.png" alt="Cult of Bulldada symbol">
-  <img class="symbol symbol--bottom-right"src="assets/img/symbol.png" alt="Cult of Bulldada symbol">
-  <img class="symbol symbol--bottom-left"src="assets/img/symbol.png" alt="Cult of Bulldada symbol">
-  <div class="emblem">
-    <img class="emblem__item" src="assets/img/emblem-2.svg">
-  </div>
-  <main class="main-content">
+    <a class="navigation__link link" href="">Menu</a>
+    <div class="fade-in fade-in__symbols">
+      <img class="symbol symbol--top-left" src="assets/img/symbol.png" alt="Cult of Bulldada symbol">
+      <img class="symbol symbol--top-right"src="assets/img/symbol.png" alt="Cult of Bulldada symbol">
+      <img class="symbol symbol--bottom-right"src="assets/img/symbol.png" alt="Cult of Bulldada symbol">
+      <img class="symbol symbol--bottom-left"src="assets/img/symbol.png" alt="Cult of Bulldada symbol">
+    </div>
+    <div class="emblem fade-in fade-in__emblem">
+      <img class="emblem__item" src="assets/img/emblem-2.svg">
+    </div>
+  <main class="main-content fade-in fade-in__main">
     <div class="center">
       <nav class="main-navigation">
         <ul class="main-navigation__list">
@@ -22,5 +24,4 @@
       <img class="logo "src="assets/img/logo-final.png" alt="Logotype of Cult of Bulldada Kazakhstan">
     </div>
   </main>
-</body>
 <?php @include 'footer.php'; ?>
