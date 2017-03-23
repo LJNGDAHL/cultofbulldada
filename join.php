38a54b46230409a6c15f99709206947cb5dcf22d
Katarina Ljungdahl <katarina@folkungagatan54.se>
2017-03-23 10:36:05 +0100

    Sorted out initial styling on all pages

--- join.php
+++ join.php
@@ -1,16 +1,22 @@
 <?php @include 'header.php'; ?>
-<body class="tb-content">
+<body class="content-body content-body__join">
   <?php @include 'navigation.php'; ?>
   <a href="../cultofbulldada/" class="link link--logo"><img class="symbol tb-content__logo" src="assets/img/logo-final.png" alt="Logotype of Cult of Bulldada Kazakhstan"></a>
-  <h1 class="tb-content__headline">Join us</h1>
-  <p class="tb-content__paragraph">
-  If you are what they call “different”<br>
-  If you think we’re entering a new Dark Ages<br>
-  If you see the universe as one vast morbid sense of humor<br><br>
-  If you are looking for an inherently bogus religion that will condone superior<br> degeneracy and tell you that you are “above” everyone else<br><br>
-  If you can help us with a donation</p>
-  <p class="tb-content__paragraph">We the Cult of bulldada of the post-human race, in order to preserve life on this planet from immanent destruction, reestablish intelligence, cast out the False Prophets, smother forever the fear of fear Itself, receive the questionable gifts of the Beforelife and of the Space Dwellers, become as Overmen, unmask the Conspiracy, grip the reigns of human evolution, control reproduction and mutation, decipher the Code, placate the Stark Fist of Removal, achieve Time Control, see That Which Must Come To Pass, become as gods, find the Twins with the Scissors of Sight, avert the Rupture of the Equilibrium, and secure the financial blessings of Jehovah 1 the God of Wrath for ourselves and our descendants, do hereby admit ourselves as such and thus ordain this Advertisement of the SubGenius Race of Earth.</p>
-  <p class="tb-content__paragraph">YOU MAY NOW STOP READING FOREVER</p>
+  <div class="center-content">
+    <h1 class="tb-content__headline">Join us</h1>
+  </div>
+  <div class="tb-content">
+    <div class="center-content">
+      <p class="tb-content__paragraph">
+      If you are what they call “different”<br>
+      If you think we’re entering a new Dark Ages<br>
+      If you see the universe as one vast morbid sense of humor<br><br>
+      If you are looking for an inherently bogus religion that will condone superior<br> degeneracy and tell you that you are “above” everyone else<br><br>
+      If you can help us with a donation</p>
+      <p class="tb-content__paragraph">We the Cult of bulldada of the post-human race, in order to preserve life on this planet from immanent destruction, reestablish intelligence, cast out the False Prophets, smother forever the fear of fear Itself, receive the questionable gifts of the Beforelife and of the Space Dwellers, become as Overmen, unmask the Conspiracy, grip the reigns of human evolution, control reproduction and mutation, decipher the Code, placate the Stark Fist of Removal, achieve Time Control, see That Which Must Come To Pass, become as gods, find the Twins with the Scissors of Sight, avert the Rupture of the Equilibrium, and secure the financial blessings of Jehovah 1 the God of Wrath for ourselves and our descendants, do hereby admit ourselves as such and thus ordain this Advertisement of the SubGenius Race of Earth.</p>
+      <p class="tb-content__paragraph">YOU MAY NOW STOP READING FOREVER</p>
+    </div>
+  </div>
 </body>
 <?php // TODO: ADD Register form ?>
 <?php @include 'footer.php'; ?>
--- mission.php
+++ mission.php
@@ -1,18 +1,24 @@
 <?php @include 'header.php'; ?>
-<body class="tb-content">
+<body class="content-body content-body__mission">
   <?php @include 'navigation.php'; ?>
   <a href="../cultofbulldada/" class="link link--logo"><img class="symbol tb-content__logo" src="assets/img/logo-final.png" alt="Logotype of Cult of Bulldada Kazakhstan"></a>
-  <h1 class="tb-content__headline">Our Mission</h1>
-  <p class="tb-content__paragraph">
-    <img class="tb-content__emblem" src="assets/img/emblem-2.svg">This is a certified religion of scorn and vengeance directed at all of them, the enemies of us Outsiders. It is “self-help” thorugh scoffing and blaspheming, frenzied fornication and the Tumping of Graven Images. The Church provides answers and miracles in the service of sureavolution.</p>
-  <p class="tb-content__paragraph">The Church of the Cult of Bulldada is the ultimate secret order, the superior brain cult for those who “know better” but who demand in the lust for grins a spectacular, special-effects-laden belief system — a ‘stuporstition.’</p>
-  <p class="tb-content__paragraph">
-    The Goal: Front End Development<br>
-    The Method: The casting out of false prophets<br>
-    The Weapon: Time Control<br>
-    The Motto: Fuck them if they can’t take a joke<br>
-  </p>
-  <p class="tb-content__paragraph">This religion, devised by precise mathematical formulae, weirder than the most obscure UFO/Atlantis cults, is invaluable to all superior renegades who, at any time, are justifiably on the edge of insanity and can be made whole only by developing a seventh sense of bludgeon humor.</p>
+  <div class="center-content">
+    <h1 class="tb-content__headline">Our Mission</h1>
+  </div>
+  <div class="tb-content">
+    <div class="center-content">
+      <p class="tb-content__paragraph">
+        <img class="tb-content__emblem" src="assets/img/emblem-2.svg">This is a certified religion of scorn and vengeance directed at all of them, the enemies of us Outsiders. It is “self-help” thorugh scoffing and blaspheming, frenzied fornication and the Tumping of Graven Images. The Church provides answers and miracles in the service of sureavolution.</p>
+      <p class="tb-content__paragraph">The Church of the Cult of Bulldada is the ultimate secret order, the superior brain cult for those who “know better” but who demand in the lust for grins a spectacular, special-effects-laden belief system — a ‘stuporstition.’</p>
+      <p class="tb-content__paragraph">
+        The Goal: Front End Development<br>
+        The Method: The casting out of false prophets<br>
+        The Weapon: Time Control<br>
+        The Motto: Fuck them if they can’t take a joke<br>
+      </p>
+      <p class="tb-content__paragraph">This religion, devised by precise mathematical formulae, weirder than the most obscure UFO/Atlantis cults, is invaluable to all superior renegades who, at any time, are justifiably on the edge of insanity and can be made whole only by developing a seventh sense of bludgeon humor.</p>
+    </div>
+  </div>
 </body>
 <?php // TODO: ADD AMBASSADOR ?>
 <?php @include 'footer.php'; ?>
