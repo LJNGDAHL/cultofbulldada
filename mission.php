f9fd4e2dce29cf6c58ae12497e52c20d5e595484
Katarina Ljungdahl <katarina@folkungagatan54.se>
2017-03-23 14:18:46 +0100

    Added an embedded video and quote styling. WIP

--- mission.php
+++ mission.php
@@ -10,15 +10,14 @@
       <p class="tb-content__paragraph">
         <img class="tb-content__emblem" src="assets/img/emblem-2.svg">This is a certified religion of scorn and vengeance directed at all of them, the enemies of us Outsiders. It is “self-help” thorugh scoffing and blaspheming, frenzied fornication and the Tumping of Graven Images. The Church provides answers and miracles in the service of sureavolution.</p>
       <p class="tb-content__paragraph">The Church of the Cult of Bulldada is the ultimate secret order, the superior brain cult for those who “know better” but who demand in the lust for grins a spectacular, special-effects-laden belief system — a ‘stuporstition.’</p>
-      <p class="tb-content__paragraph">
-        The Goal: Front End Development<br>
-        The Method: The casting out of false prophets<br>
-        The Weapon: Time Control<br>
-        The Motto: Fuck them if they can’t take a joke<br>
+      <p class="tb-content__paragraph quote">
+        <span class="purple bold uppercase">The Goal</span>: Front End Development<br>
+        <span class="purple bold uppercase">The Method</span>: The casting out of false prophets<br>
+        <span class="purple bold uppercase">The Weapon</span>: Time Control<br>
+        <span class="purple bold uppercase">The Motto</span>: Fuck them if they can’t take a joke<br>
       </p>
       <p class="tb-content__paragraph">This religion, devised by precise mathematical formulae, weirder than the most obscure UFO/Atlantis cults, is invaluable to all superior renegades who, at any time, are justifiably on the edge of insanity and can be made whole only by developing a seventh sense of bludgeon humor.</p>
     </div>
   </div>
 </body>
-<?php // TODO: ADD AMBASSADOR ?>
 <?php @include 'footer.php'; ?>
