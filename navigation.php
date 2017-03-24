bb3c8b5f72336321a44341bd4ab4d7faaa64f097
Katarina Ljungdahl <katarina@folkungagatan54.se>
2017-03-24 10:24:51 +0100

    Cleaned up code

--- navigation.php
+++ navigation.php
@@ -2,7 +2,7 @@
 <nav class="main-navigation" id="navigation">
   <a class="link navigation__link" href="#menu">Exit</a>
   <div class="symbol-container">
-    <img class="symbol symbol__nav symbol__nav--top" src="assets/img/cob-symbol.svg" alt="Cult of Bulldada symbol">
+    <img class="symbol symbol__nav symbol__nav--top" src="assets/img/symbol-landscape.svg" alt="Cult of Bulldada symbol">
     <ul class="main-navigation__list">
       <li><a class="link link--nav" href="../cultofbulldada/">Home</a></li>
       <li><a class="link link--nav" href="about.php">About</a></li>
@@ -11,6 +11,6 @@
       <li><a class="link link--nav" href="bulldada.php">Bulldada</a></li>
       <li><a class="link link--nav" href="join.php">Join us</a></li>
     </ul>
-    <img class="symbol symbol__nav symbol__nav--bottom" src="assets/img/cob-symbol.svg" alt="Cult of Bulldada symbol">
+    <img class="symbol symbol__nav symbol__nav--bottom" src="assets/img/symbol-landscape.svg" alt="Cult of Bulldada symbol">
   </div>
 </nav>
